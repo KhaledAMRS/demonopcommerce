75cf88762905cadd5adb231c6a37d41770e9b6ca
khaled ramadan <khaled-shabaik@La3eb.com>
2022-11-22 21:41:06 +0200

    Initial commit

---
+++ src/test/java/org/example/stepDefs/D01_Register.java
@@ -0,0 +1,20 @@
+package org.example.stepDefs;
+
+import io.cucumber.java.en.Given;
+import org.example.pages.P01_register;
+import org.openqa.selenium.By;
+
+import static org.example.stepDefs.Hooks.driver;
+
+public class D01_Register {
+
+    P01_register register = new P01_register();
+
+    @Given("user go to register page")
+    public void step1()
+    {
+        register.regLink().click();
+    }
+
+
+}
---
+++ src/test/java/org/example/stepDefs/Hooks.java
@@ -0,0 +1,43 @@
+package org.example.stepDefs;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Hooks {
+
+public static WebDriver driver;
+
+    @Before
+    public void openBrowser()
+    {
+      //1- Bridge
+        WebDriverManager.chromedriver().setup();
+        //or
+        // System.setProperty()
+
+        //2- new object from Chrome
+        driver = new ChromeDriver();
+
+        //3- Configuration
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //4- Navigate
+        driver.navigate().to("https://demo.nopcommerce.com/");
+
+    }
+
+    @After
+    public void quitDriver() throws InterruptedException {
+
+        Thread.sleep(3000);
+        driver.quit();
+
+    }
+
+}
